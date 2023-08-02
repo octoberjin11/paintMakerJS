67db8a4ceca8afea07f78615f16a96332b118a43
heejoung <octoberjin11@gmail.com>
2023-08-02 23:26:29 +0900

    Drawing Project Two

--- app.js
+++ app.js
@@ -8,14 +8,16 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 집 만들기
-ctx.fillRect(200, 200, 50, 200); //왼쪽 벽 만들기
-ctx.fillRect(400, 200, 50, 200); //오른쪽 벽 만들기
-// ctx.lineWidth = 2; //선 굵기 조절
-// ctx.strokeRect(300, 300, 50, 100); //문 만들기. strokeRect()는 선만 그려주고 채워주지 않는.
-ctx.fillRect(300, 300, 50, 100); //문 만들기
-ctx.fillRect(200, 200, 200, 20); //천장 만들기
-ctx.moveTo(200, 200); // 지붕 만들기 위해 연필(좌표) 이동
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+// 사람 그리기
+ctx.fillRect(210 - 40, 200 - 30, 15, 100); //왼팔
+ctx.fillRect(350 - 40, 200 - 30, 15, 100); //오른팔
+ctx.fillRect(260 - 40, 200 - 30, 60, 200); //몸통
+
+ctx.arc(250, 100, 50, 0, 2 * Math.PI); //열굴
+ctx.fill();
+
+ctx.beginPath(); //색이 다른 path를 만들어주기 위해 새로운 path를 만들어줌.
+ctx.fillStyle = "white";
+ctx.arc(260 + 10, 80, 8, Math.PI, 2 * Math.PI);
+ctx.arc(220 + 10, 80, 8, Math.PI, 2 * Math.PI);
 ctx.fill();
