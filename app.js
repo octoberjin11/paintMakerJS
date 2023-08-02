ce21cd64e73306cc9264adde0b8ecd41ddcee543
heejoung <octoberjin11@gmail.com>
2023-08-02 23:05:34 +0900

    moveTo and lineTo

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-// 사각형 선 그리기
-ctx.rect(50, 50, 100, 100);
-ctx.rect(150, 150, 100, 100);
-ctx.rect(250, 250, 100, 100);
-// 위의 코드까지만 작성하면 선의 색이 적용되지 않아서 보이지 않는다.
-// 다음 줄에 ctx.stroke() / ctx.fill() 해서 테두리만 그리거나 채울 수 있음.
-ctx.fill();
+/*
+moveTo(x, y); -> 브러쉬의 좌표를 움직여줌
+lineTo(x, y) -> 라인을 그려줌
 
-// 끊어가기를 원하는 곳 맨 앞에 ctx.beginPath();추가해 새 경로 만들기.
-ctx.beginPath();
-ctx.rect(350, 350, 100, 100);
-ctx.rect(450, 450, 100, 100);
-ctx.fillStyle = "red";
+- 수평인 직선을 그리려면 두 y값이 같아야 한다
+- 라인이 끝난 점이 다음에 시작하는 브러쉬 좌표이다
+- 정리하자면 fillRect = fill+Rect = fill + (moveTo+lineTo)
+*/
+
+ctx.moveTo(50, 50);
+ctx.lineTo(150, 50);
+ctx.lineTo(150, 150);
+ctx.lineTo(50, 150);
+ctx.lineTo(50, 50);
 ctx.fill();
